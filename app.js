324050f91c0deafe3e9253a823b8100fb6ce68de
Savannah <122945555+smithsav@users.noreply.github.com>
2024-04-30 23:00:26 +0000

    4/30/2024
    updated based on powerpoint

--- app.js
+++ app.js
@@ -21,6 +21,34 @@ app.use((req, res, next) => {
 // view engine
 app.set('view engine', 'ejs');
 
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://sdev255:password255@cluster0.baie5ob.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
 // database connection
 const dbURI = ('mongodb+srv://testUser:pass1234@nodeauthtutorial.osde3dd.mongodb.net/?retryWrites=true&w=majority&appName=nodeAuthTutorial');
 mongoose.connect(dbURI)
@@ -40,3 +68,5 @@ app.use('/cart', requireAuth, cartRoutes);
 app.use((req, res) => {
     res.status(404).render('404', { title: '404' });
 });
+
+
